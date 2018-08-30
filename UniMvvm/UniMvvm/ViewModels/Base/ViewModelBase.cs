1aad241e9cce8967ab8df798dc5dfc7b51c4cc8f
Ramin <ramin.mh2000@gmail.com>
2018-08-30 08:31:31 -0700

    Lazy Loading Services in viewmodel base

--- UniMvvm/UniMvvm/ViewModels/Base/ViewModelBase.cs
+++ UniMvvm/UniMvvm/ViewModels/Base/ViewModelBase.cs
@@ -1,12 +1,28 @@
 ﻿using System.Threading.Tasks;
+using UniMvvm.DataServices.Interfaces;
 using UniMvvm.Services.Interfaces;
 
 namespace UniMvvm.ViewModels.Base
 {
     public abstract class ViewModelBase : ExtendedBindableObject
     {
-        protected readonly IDialogService DialogService;
-        protected readonly INavigationService NavigationService;
+        private IRequestProvider httProvider;
+        protected IRequestProvider HttProvider
+        {
+            get { return httProvider ?? (httProvider = ViewModelLocator.Instance.Resolve<IRequestProvider>()); }
+        }
+
+        private IDialogService dialogService;
+        protected IDialogService DialogService
+        {
+            get { return dialogService ?? (dialogService = ViewModelLocator.Instance.Resolve<IDialogService>()); }
+        }
+
+        private INavigationService navigationService;
+        protected INavigationService NavigationService
+        {
+            get { return navigationService ?? (navigationService = ViewModelLocator.Instance.Resolve<INavigationService>()); }
+        }
 
         private bool _isBusy;
 
@@ -24,10 +40,10 @@ namespace UniMvvm.ViewModels.Base
             }
         }
 
+       
+
         public ViewModelBase()
         {
-            DialogService = ViewModelLocator.Instance.Resolve<IDialogService>();
-            NavigationService = ViewModelLocator.Instance.Resolve<INavigationService>();
         }
 
         public virtual Task InitializeAsync(object navigationData)
